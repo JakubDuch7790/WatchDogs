2250704319f2ea40cfc2a33628a052cdf45bcc69
Jakub Duch <jakub.duch23@gmail.com>
2024-07-22 22:17:37 +0200

    Trying to finish authentication and connect to API

--- src/WatchDogs.Application/Program.cs
+++ src/WatchDogs.Application/Program.cs
@@ -2,6 +2,11 @@
 
 
 using Contracts;
+using Infrastructure.DxTrade;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +19,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddHttpClient("myClient", client => {
+builder.Services.AddHttpClient("DxTradeAuthenticatorClient", client => {
     client.BaseAddress = new Uri("https://dxtrade.ftmo.com/dxsca-web/");
 });
 
+//builder.Services.AddTransient<IDxTradeAuthenticator, DxTradeAuthenticator>();
+builder.Services.AddSingleton<IDxTradeAuthenticator, DxTradeAuthenticator>();
+
+
+//builder.Services.AddHttpClient<IDxTradeAuthenticator, DxTradeAuthenticator>(client =>
+//    client.BaseAddress = new Uri("https://dxtrade.ftmo.com/dxsca-web/"));
+
 var app = builder.Build();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+
+    var dxTradeAuthenticator = services.GetRequiredService<IDxTradeAuthenticator>();
+
+    await dxTradeAuthenticator.AuthenticateAsync();
+
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -35,3 +56,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+
--- src/WatchDogs.Infrastructure.DxTrade/DxTradeAuthenticator.cs
+++ src/WatchDogs.Infrastructure.DxTrade/DxTradeAuthenticator.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System.Net.Http;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json;
+using System.Text;
 
 namespace Infrastructure.DxTrade
 {
@@ -10,18 +13,26 @@ namespace Infrastructure.DxTrade
         private readonly IHttpClientFactory _httpClientFactory;
 
         private readonly DxTradeConnectionOptions _connectionOptions;
+
         public DxTradeAuthenticator(IOptions<DxTradeConnectionOptions> configuration, IHttpClientFactory httpClientFactory)
         {
             _connectionOptions = configuration.Value;
             _httpClientFactory = httpClientFactory;
-
         }
 
-        public Task AuthenticateAsync()
+        public async Task AuthenticateAsync()
         {
-            var client = _httpClientFactory.CreateClient();
+            var client = _httpClientFactory.CreateClient("DxTradeAuthenticatorClient");
 
-            throw new NotImplementedException();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var jsonData = JsonSerializer.Serialize(_connectionOptions);
+
+            var requestContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(client.BaseAddress + "login", requestContent);
+
+            response.EnsureSuccessStatusCode();
         }
     }
 }
