30ad36fdb8b8b27e28b7e06ee24276cb75e05369
Jakub Duch <jakub.duch23@gmail.com>
2024-07-25 17:20:52 +0200

    Test project added to solution

---
+++ src/WatchDogs.Test/DxTradeAuthenticatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Contracts;
+using Infrastructure.DxTrade;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchDogs.Test;
+public class DxTradeAuthenticatorTests
+{
+    private const string SessionTokenHeaderName = "JSESSIONID";
+    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock;
+    private readonly Mock<ISessionTokenStorage> _sessionTokenStorageMock;
+    private readonly DxTradeConnectionOptions _connectionOptions;
+    private readonly DxTradeAuthenticator _authenticator;
+
+    [Fact]
+    public void AuthenticateAsync_Should()
+    {
+        
+    }
+    
+}
+
+
