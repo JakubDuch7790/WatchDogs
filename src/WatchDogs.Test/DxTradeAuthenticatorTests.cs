b72ea961f15005475d91e3e0ebf8a48e751476cd
Jakub Duch <jakub.duch23@gmail.com>
2024-07-26 16:10:03 +0200

    Constructor modified based on comments from proffesional programmer

--- src/WatchDogs.Test/DxTradeAuthenticatorTests.cs
+++ src/WatchDogs.Test/DxTradeAuthenticatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Infrastructure.DxTrade;
+using Microsoft.Extensions.Options;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -10,20 +11,28 @@ using System.Threading.Tasks;
 namespace WatchDogs.Test;
 public class DxTradeAuthenticatorTests
 {
-    private const string SessionTokenHeaderName = "JSESSIONID";
-    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock;
-    private readonly Mock<ISessionTokenStorage> _sessionTokenStorageMock;
-    private readonly DxTradeConnectionOptions _connectionOptions;
+    private readonly Mock<IHttpClientFactory> _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+    private readonly Mock<ISessionTokenStorage> _sessionTokenStorageMock = new Mock<ISessionTokenStorage>();
     private readonly DxTradeAuthenticator _authenticator;
 
     public DxTradeAuthenticatorTests()
     {
-        _httpClientFactoryMock = new Mock<IHttpClientFactory>();
-        _sessionTokenStorageMock = new Mock<ISessionTokenStorage>();
+        var optionsMock = new Mock<IOptions<DxTradeConnectionOptions>>();
+
+        var connectionOptions = new DxTradeConnectionOptions
+        {
+            Vendor = "ftmo",
+            Username = "username",
+            Password = "password"
+        };
+
+        optionsMock.Setup(o => o.Value).Returns(connectionOptions);
+
+        _authenticator = new DxTradeAuthenticator(optionsMock.Object, _httpClientFactoryMock.Object, _sessionTokenStorageMock.Object);
     }
 
     [Fact]
-    public async Task AuthenticateAsync_ShouldGenerateAndStoreSessionToken()
+    public async Task AuthenticateAsync_ShouldGenerateAndStoreSessionToken_InCaseOfSuccessfullResponse()
     {
         //Arrange
 
