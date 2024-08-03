236f432e1e6056fbae9051c8a9cfa6e6bac216c1
Jakub Duch <jakub.duch23@gmail.com>
2024-08-03 12:57:26 +0200

    ITrade Repo added

--- src/WatchDogs.Contracts/ITradeRepository.cs
+++ src/WatchDogs.Contracts/ITradeRepository.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Contracts;
+namespace WatchDogs.Contracts;
 
-public interface IRepository
+public interface ITradeRepository
 {
-    Task CreateAsync();
-    Task ListAsync();
+    Task GetTradesAsync();
 }
