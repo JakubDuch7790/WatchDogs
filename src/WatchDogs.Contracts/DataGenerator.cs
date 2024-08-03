a190c746de0e5c8d0d834896765eb6635ffabce5
Jakub Duch <jakub.duch23@gmail.com>
2024-08-03 20:19:12 +0200

    Implementing Bogus data generator

---
+++ src/WatchDogs.Contracts/DataGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+
+namespace WatchDogs.Contracts;
+
+public class DataGenerator
+{
+    // This holds an information or set of rules for generating fake data for TradeModel
+
+    Faker<TradeModel> tradeModelFake;
+
+    public DataGenerator()
+    {
+        // This field is optional and it allows us to replicate the results (everytime we runs an application, the app will generate same results)
+        Randomizer.Seed = new Random(123);
+
+        tradeModelFake = new Faker<TradeModel>()
+            .RuleFor(u => u.Currency, f => f.Finance.Currency().Code);
+    }
+
+}
---
+++ src/WatchDogs.Contracts/TradeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+
+namespace WatchDogs.Contracts;
+
+public record TradeModel(
+
+    Guid DealsGuid,
+    decimal AcountBalance,
+    TradeAction Action,
+    DateTimeOffset TimeStamp,
+    string Currency,
+    decimal Lot
+    );
