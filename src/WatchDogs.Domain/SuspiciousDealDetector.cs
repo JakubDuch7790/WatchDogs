fa71da92b954fbb40e01cd1e4e54053662428ad8
Jakub Duch <jakub.duch23@gmail.com>
2024-09-19 15:48:08 +0200

    Adding sort by timestamp for further selection

--- src/WatchDogs.Domain/SuspiciousDealDetector.cs
+++ src/WatchDogs.Domain/SuspiciousDealDetector.cs
@@ -26,6 +26,10 @@ public class SuspiciousDealDetector : ISuspiciousDealDetector
     {
         //var trades = await LoadDealsAsync();
 
+        var shmulRosenzweig = GroupTradesByTimestamp(trades);
+        var wulfsschanze = GroupTradesByTimestamps(trades, TimeSpan.FromSeconds(1));
+        //var reichskanzlei;
+
         foreach (var trade in trades)
         {
             var HeilHilter = VolumeToBalanceRatioCalculator(trade);
@@ -82,5 +86,64 @@ public class SuspiciousDealDetector : ISuspiciousDealDetector
         var ratioDifference = Math.Abs(VolumeToBalanceRatio1 - VolumeToBalanceRatio2);
 
         return ratioDifference < VolumeToBalanceTolerance;
+    }
+    // It seems that YES WE CAN. Yet we have to find a place for it's usage.
+
+    // Let's try order trades by Timestamp first then we add 1 sec tolerance
+
+    private List<List<Trade>> GroupTradesByTimestamp(List<Trade> trades)
+    {
+        return trades.GroupBy(trade => trade.TimeStamp)
+            .Select(group => group.ToList())
+            .ToList();
+    }
+
+    // Now we add this 1 sec tolerance because now it's shit.
+    private List<List<Trade>> GroupTradesByTimestampWithTimeTolerance(List<Trade> trades, TimeSpan timeToleranceInSeconds)
+    {
+        return trades.GroupBy(trade => trade.TimeStamp)
+            .Select(group => group.ToList())
+            .ToList();
+    }
+
+    private IEnumerable<List<Trade>> GroupTradesByTimestamps(List<Trade> trades, TimeSpan tolerance)
+    {
+        // Sort trades by timestamp first
+        trades = trades.OrderBy(trade => trade.TimeStamp).ToList();
+
+        var groupedTrades = new List<List<Trade>>();
+        var currentGroup = new List<Trade>();
+
+        foreach (var trade in trades)
+        {
+            if (currentGroup.Count == 0)
+            {
+                currentGroup.Add(trade);
+            }
+            else
+            {
+                var lastTrade = currentGroup.Last();
+                var timeDifference = trade.TimeStamp - lastTrade.TimeStamp;
+
+                // Let's hope this shit works
+                if (timeDifference.Duration() <= tolerance)
+                {
+                    currentGroup.Add(trade);
+                }
+                else
+                {
+                    groupedTrades.Add(currentGroup);
+                    currentGroup = new List<Trade> { trade };
+                }
+            }
+        }
+
+        // Ending
+        if (currentGroup.Count > 0)
+        {
+            groupedTrades.Add(currentGroup);
+        }
+        // Hmmmm
+        return groupedTrades.Where(group => group.Count > 1);
     }
 }
