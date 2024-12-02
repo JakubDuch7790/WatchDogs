e7d11e76c66cb775441e7a40c271cc57ff9d0a44
Jakub Duch <jakub.duch23@gmail.com>
2024-12-02 22:52:34 +0100

    .

--- src/WatchDogs.Test/FakeSourceWatcherTests.cs
+++ src/WatchDogs.Test/FakeSourceWatcherTests.cs
@@ -32,8 +32,8 @@ namespace WatchDogs.Test
             var mockUnitOfWork = new Mock<IUnitOfWork>();
             var mockTradeInserter = new Mock<ITradeInserter>();
 
-            mockUnitOfWork.SetupGet(x => x.DataInserter).Returns(mockTradeInserter.Object);
-            mockTradeInserter.SetupGet(x => x.InsertTradeDatatoDbAsync(It.IsAny<IEnumerable<Trade>>())).Returns(Task.CompletedTask);
+            mockUnitOfWork.Setup(x => x.DataInserter).Returns(mockTradeInserter.Object);
+            mockTradeInserter.Setup(x => x.InsertTradeDatatoDbAsync(It.IsAny<IEnumerable<Trade>>())).Returns(Task.CompletedTask);
 
             _unitOfWorkFactory.Setup(f => f.Create()).Returns(mockUnitOfWork.Object);
 
